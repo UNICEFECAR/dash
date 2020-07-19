cc404ed866e7401c7de2b7fe8096857074bc6d31
James Cranwell-Ward <jcranwellward@gmail.com>
2020-07-19 22:53:51 +0200

    Update education.py

--- transmonee_dashboard/src/transmonee_dashboard/pages/education.py
+++ transmonee_dashboard/src/transmonee_dashboard/pages/education.py
@@ -41,14 +41,12 @@ indicators_dict = {
         "name": "Proportion of children at the end of primary education reaching minimum proficiency in math",
         "compare": {
             "EDU_SDG_STU_L1_GLAST_READING": "Proportion of children at the end of primary education reaching minimum proficiency in reading",
-            "EDUNF_NERA_L1_PT": "Adjusted net enrolment: primary education"
+            "EDUNF_NERA_L1_PT": "Adjusted net enrolment: primary education",
         },
     },
     "EDU_SDG_STU_L1_G2OR3_READING": {
         "name": "Proportion of children in grade 2 or 3 reaching minimum proficiency in math",
-        "compare": {
-            "EDUNF_ROFST_L1": "Out of school children rate: Primary education"
-        },
+        "compare": {"EDUNF_ROFST_L1": "Out of school children rate: Primary education"},
     },
     "EDU_SDG_STU_L1_G2OR3_MATH": {
         "name": "Proportion of children in grade 2 or 3 reaching minimum proficiency in math",
@@ -56,7 +54,7 @@ indicators_dict = {
             "EDU_SDG_STU_L1_GLAST_READING": "Proportion of children at the end of primary education reaching minimum proficiency in reading"
         },
     },
-    "EDU_SDG_GER_L01": {"compare": {}}
+    "EDU_SDG_GER_L01": {"compare": {}},
 }
 
 codes = [
@@ -98,13 +96,21 @@ years = [i for i in range(2008, 2020)]
 
 indicators = data["Indicator"].unique()
 
-reading = go.Figure(go.Indicator(
-    mode = "number",
-    value = data.query("CODE == 'EDU_SDG_STU_L2_READING' & Sex == 'Total'").mean(),
-    number = {'prefix': "$"},
-px.bar(
-    data.query("CODE == 'EDU_SDG_STU_L2_READING' & Sex == 'Total'").groupby(['Sex']).mean().reset_index(), 
-    x='Sex', y='OBS_VALUE', color='Sex', range_y=[0, 100], le)
+regions = [
+    {
+        "label": "Western Balkans",
+        "value": "Albania, Bosnia and Herzegovina, Kosovo, North Macedonia, Montenegro, Serbia",
+    },
+    {"label": "EU countries of ECAR", "value": "Bulgaria, Croatia, Romania"},
+    {"label": "B+M+U", "value": "Belarus, Moldova, Ukraine"},
+    {"label": "Caucasus", "value": "Armenia, Azerbaijan, Georgia"},
+    {"label": "Turkey", "value": "Turkey"},
+    {
+        "label": "Central Asia",
+        "value": "Kazakhstan, Kyrgyzstan, Tajikistan, Turkmenistan, Uzbekistan",
+    },
+]
+
 
 def indicator_card(indicator):
     mean_value = (
@@ -116,9 +122,7 @@ def indicator_card(indicator):
     name = data[data["CODE"] == indicator]["Indicator"].unique()[0]
     fig = go.Figure(
         go.Indicator(
-            value=mean_value,
-            number={"suffix": "%"},
-            domain = {'x': [0, 1], 'y': [0, 1]},
+            value=mean_value, number={"suffix": "%"}, domain={"x": [0, 1], "y": [0, 1]},
         )
     )
     fig.update_layout(height=200)
@@ -126,13 +130,7 @@ def indicator_card(indicator):
         [
             dbc.CardHeader("Card header"),
             dbc.CardBody(
-                [
-                    dcc.Graph(figure=fig),
-                    html.P(
-                        name,
-                        className="card-text",
-                    ),
-                ]
+                [dcc.Graph(figure=fig), html.P(name, className="card-text",),]
             ),
         ],
         color="primary",
@@ -170,13 +168,21 @@ def get_layout(**kwargs):
                                             className="dcc_control",
                                         ),
                                         html.P(
-                                            "Filter by Country:",
+                                            "Filter by Region:",
                                             className="control_label",
                                         ),
                                         dcc.Dropdown(
+                                            id="region_selector",
+                                            options=regions,
+                                            className="dcc_control",
+                                        ),
+                                        html.P(
+                                            "Filter by Country:",
+                                            className="control_label",
+                                        ),
+                                        dbc.Checklist(
                                             id="country_selector",
                                             options=county_options,
-                                            multi=True,
                                             value=[
                                                 item["value"] for item in county_options
                                             ],
@@ -194,33 +200,39 @@ def get_layout(**kwargs):
                         [
                             dbc.Row(
                                 [
-                                    dbc.Col(
-                                        indicator_card("EDU_SDG_GER_L01")
-                                    ),
-                                    dbc.Col(
-                                        indicator_card("EDU_SDG_STU_L2_READING")
-                                    ),
-                                    dbc.Col(
-                                        indicator_card("EDUNF_LR_L02")
-                                    ),
-                                    dbc.Col(
-                                        indicator_card("EDU_SDG_GER_L01")
-                                    ),
+                                    dbc.Col(indicator_card("EDU_SDG_GER_L01")),
+                                    dbc.Col(indicator_card("EDU_SDG_STU_L2_READING")),
+                                    dbc.Col(indicator_card("EDUNF_LR_L02")),
+                                    dbc.Col(indicator_card("EDU_SDG_GER_L01")),
                                 ],
                                 # className="mb-4",
                             ),
                             html.Br(),
                             dbc.Card(
-                                dbc.CardBody(dcc.Graph(id="count_graph")),
+                                dbc.CardBody(
+                                    [
+                                        dcc.Graph(id="count_graph"),
+                                        dbc.ButtonGroup(
+                                            [
+                                                dbc.Button("Left"),
+                                                dbc.Button("Middle"),
+                                                dbc.Button("Right"),
+                                            ],
+                                            size="sm",
+                                            style='p'
+                                            
+                                        )
+                                    ]
+                                ),
                                 # id="countGraphContainer",
                                 # className="pretty_container",
                             ),
                             html.Br(),
                         ],
-                # className="row flex-display",
+                        # className="row flex-display",
                         id="right-column",
-                                # className="eight columns",
-                        width=8
+                        # className="eight columns",
+                        width=8,
                     ),
                 ]
             ),
@@ -333,7 +345,7 @@ def geocode_address(address):
 
 
 # Slider -> count graph
-@ app.callback(Output("year_slider", "value"), [Input("count_graph", "selectedData")])
+@app.callback(Output("year_slider", "value"), [Input("count_graph", "selectedData")])
 def update_year_slider(count_graph_selected):
 
     if count_graph_selected is None:
@@ -345,27 +357,41 @@ def update_year_slider(count_graph_selected):
 
 @app.callback(
     Output("count_graph", "figure"),
-    [Input("year_slider", "value"), Input("country_selector", "value")],
+    [Input("year_slider", "value"), ],
     #     [State("lock_selector", "value"), State("count_graph", "relayoutData")],
 )
 def make_map(year_slider, country_selector):
 
     px.set_mapbox_access_token(mapbox_access_token)
     static = {
-        'Country': ['Albania', 'Belarus', 'Bosnia and Herzegovina', 'Bulgaria', 'Croatia', 
-                    'Georgia, Europe, Asia', 'Kazakhstan', 'Kosovo', 'Moldova', 'Montenegro', 
-                    'North Macedonia', 'Romania', 'Serbia', 'Turkey', 'Ukraine'],
-        'Reading': [52, 23, 54, 47, 22, 64, 64, 79, 43, 44, 55, 41, 38, 26, 26],
-        'Math': [42, 29, 58, 44, 31, 61, 49, 77, 50, 46, 61, 47, 40, 37, 36],
-        'Science': [47, 24, 57, 47, 25, 64, 60, 77, 43, 48, 49, 44, 38, 25, 26]
+        "Country": [
+            "Albania",
+            "Belarus",
+            "Bosnia and Herzegovina",
+            "Bulgaria",
+            "Croatia",
+            "Georgia, Europe, Asia",
+            "Kazakhstan",
+            "Kosovo",
+            "Moldova",
+            "Montenegro",
+            "North Macedonia",
+            "Romania",
+            "Serbia",
+            "Turkey",
+            "Ukraine",
+        ],
+        "Reading": [52, 23, 54, 47, 22, 64, 64, 79, 43, 44, 55, 41, 38, 26, 26],
+        "Math": [42, 29, 58, 44, 31, 61, 49, 77, 50, 46, 61, 47, 40, 37, 36],
+        "Science": [47, 24, 57, 47, 25, 64, 60, 77, 43, 48, 49, 44, 38, 25, 26],
     }
-    df = pd.DataFrame(static, columns = ['Country', 'Reading', 'Math', 'Science'])
+    df = pd.DataFrame(static, columns=["Country", "Reading", "Math", "Science"])
     # 2- - create location column
     df[["longitude", "latitude"]] = df.apply(
         lambda row: pd.Series(geocode_address(row["Country"])), axis=1
     )
 
-    fig=px.scatter_mapbox(
+    fig = px.scatter_mapbox(
         df,
         lat="latitude",
         lon="longitude",
@@ -375,14 +401,14 @@ def make_map(year_slider, country_selector):
         color_continuous_scale=px.colors.sequential.Jet,
         size_max=30,
         zoom=2.5,
-        title='Percentage of students performing below level 2/basic proficiency in all 3 subjects'
+        title="Percentage of students performing below level 2/basic proficiency in all 3 subjects",
     )
 
     return fig
 
 
 # Selectors -> maths graph
-@ app.callback(
+@app.callback(
     Output("maths_graph", "figure"),
     [Input("year_slider", "value"), Input("maths-xaxis-column", "value"),],
     #     [State("lock_selector", "value"), State("count_graph", "relayoutData")],
@@ -391,9 +417,16 @@ def make_maths_figure(year_slider, xaxis):
     indicator = xaxis
 
     name = data[data["CODE"] == indicator]["Indicator"].unique()[0]
-    df = data[(data["CODE"] == indicator) & (data['Sex'] != 'Total')].groupby(
-        ['CODE', 'Indicator', 'Geographic area', 'Sex']
-    ).agg({'TIME_PERIOD': 'last', 'OBS_VALUE': 'last'}).reset_index()
+    df = (
+        data[
+            (data["CODE"] == indicator)
+            & (data["Sex"] != "Total")
+            & (data["TIME_PERIOD"].isin(years))
+        ]
+        .groupby(["CODE", "Indicator", "Geographic area", "Sex"])
+        .agg({"TIME_PERIOD": "last", "OBS_VALUE": "last"})
+        .reset_index()
+    )
     # fig = go.Figure()
     # fig.add_trace(
     #     go.Bar(
@@ -413,11 +446,11 @@ def make_maths_figure(year_slider, xaxis):
     #         )
 
     fig = px.bar(
-        df, 
-        x="Geographic area", 
-        y="OBS_VALUE", 
-        color='Sex', 
-        barmode='group',
+        df,
+        x="Geographic area",
+        y="OBS_VALUE",
+        color="Sex",
+        barmode="group",
         title=name,
         text="TIME_PERIOD"
         # range_y=[0, 100]
@@ -443,7 +476,7 @@ def make_maths_figure(year_slider, xaxis):
 
 
 # Selectors -> reading graph
-@ app.callback(
+@app.callback(
     Output("reading_graph", "figure"),
     [Input("year_slider", "value"), Input("maths-xaxis-column", "value"),],
     #     [State("lock_selector", "value"), State("count_graph", "relayoutData")],
@@ -453,27 +486,33 @@ def make_reading_figure(year_slider, xaxis):
     compare = "Sex"
 
     name = data[data["CODE"] == indicator]["Indicator"].unique()[0]
-    df = data[(data["CODE"] == indicator) & (data['Sex'] != 'Total')].groupby(
-            ['CODE', 'Indicator', 'TIME_PERIOD', 'Geographic area']
-    ).agg({'TIME_PERIOD':'last', 'OBS_VALUE': 'mean'}).reset_index()
-    df = df.set_index('TIME_PERIOD')
+    df = (
+        data[
+            (data["CODE"] == indicator)
+            & (data["Sex"] != "Total")
+            & (data["TIME_PERIOD"].isin(years))
+        ]
+        .groupby(["CODE", "Indicator", "Geographic area", "TIME_PERIOD"])
+        .agg({"OBS_VALUE": "mean"})
+        .reset_index()
+    )
+    # df = df.set_index('TIME_PERIOD')
 
     fig = go.Figure()
-    for country in data['Geographic area'].unique():
+    for country in data["Geographic area"].unique():
         fig.add_trace(
             go.Scatter(
                 mode="lines+markers",
                 name=country,
-                x=years,
-                y=df[df['Geographic area'] == country]["OBS_VALUE"],
+                x=df[df["Geographic area"] == country]["TIME_PERIOD"],
+                y=df[df["Geographic area"] == country]["OBS_VALUE"],
                 line=dict(shape="spline", smoothing=1.3, width=1),
                 marker=dict(symbol="diamond-open"),
-                
             ),
         )
     fig.update_layout(
         title=name,
-        
+        # xaxis=dict(),
         #         xaxis_tickfont_size=14,
         yaxis=dict(
             title="Proportion in %",
@@ -490,7 +529,7 @@ def make_reading_figure(year_slider, xaxis):
 
 
 # Selectors -> reading graph
-@ app.callback(
+@app.callback(
     Output("pie_graph", "figure"),
     [Input("year_slider", "value"),],
     #     [State("lock_selector", "value"), State("count_graph", "relayoutData")],
