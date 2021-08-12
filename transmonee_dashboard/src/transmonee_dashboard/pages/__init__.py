745e3b3d25cf89ceba097714e097eb41fb32f847
lbassil <86417315+lbassil@users.noreply.github.com>
2021-08-12 06:52:23 +0000

    Use iso3 codes instead of country names

--- transmonee_dashboard/src/transmonee_dashboard/pages/__init__.py
+++ transmonee_dashboard/src/transmonee_dashboard/pages/__init__.py
@@ -22,16 +22,14 @@ geocoder = Geocoder(access_token=mapbox_access_token)
 
 
 def geocode_address(address):
-    """Geocode street address into lat/long."""
-    # Add this change to return the correct lat/long for the country Czech Republic (returned by SDMX as Czechia)
-    if address == "Czechia":
-        address = "Czech Republic"
+    """Geocode iso3 country code into lat/long."""
     # Set the type of address to country in order to return the lat/long of the country Georgia and not the US State!
-    response = geocoder.forward(address, types=["country"])
-    # Add this condition to avoid any exception if the country name was not found
-    if response.json()["features"]:
-        coords = response.json()["features"][0]["center"]
-        return dict(longitude=coords[0], latitude=coords[1])
+    # Had to change the ISO3 of Kosovo ==> need to check Kosovo ISO3 code returned by SDMX
+    response = geocoder.forward(
+        "KOS" if address == "XKX" else address, types=["country"]
+    )
+    coords = response.json()["features"][0]["center"]
+    return dict(longitude=coords[0], latitude=coords[1])
 
 
 codes = [
@@ -506,6 +504,66 @@ countries_dict = {
     "Uzbekistan": "Uzbekistan",
 }
 
+# a key:value dictionary of countries where the 'key' is the country name as displayed in the selection
+# tree whereas the 'value' is the country name as returned by the sdmx list: https://sdmx.data.unicef.org/ws/public/sdmxapi/rest/codelist/UNICEF/CL_COUNTRY/1.0
+countries_iso3_dict = {
+    "Albania": "ALB",
+    "Andorra": "AND",
+    "Armenia": "ARM",
+    "Austria": "AUT",
+    "Azerbaijan": "AZE",
+    "Belarus": "BLR",
+    "Belgium": "BEL",
+    "Bosnia and Herzegovina": "BIH",
+    "Bulgaria": "BGR",
+    "Croatia": "HRV",
+    "Cyprus": "CYP",
+    "Czech Republic": "CZE",
+    "Denmark": "DNK",
+    "Estonia": "EST",
+    "Finland": "FIN",
+    "France": "FRA",
+    "Georgia": "GEO",
+    "Germany": "DEU",
+    "Greece": "GRC",
+    "Holy See": "VAT",
+    "Hungary": "HUN",
+    "Iceland": "ISL",
+    "Ireland": "IRL",
+    "Italy": "ITA",
+    "Kazakhstan": "KAZ",
+    "Kosovo (UN SC resolution 1244)": "XKX",  # UNDP defines it as KOS
+    "Kyrgyzstan": "KGZ",
+    "Latvia": "LVA",
+    "Liechtenstein": "LIE",
+    "Lithuania": "LTU",
+    "Luxembourg": "LUX",
+    "Malta": "MLT",
+    "Monaco": "MCO",
+    "Montenegro": "MNE",
+    "Netherlands": "NLD",
+    "North Macedonia": "MKD",
+    "Norway": "NOR",
+    "Poland": "POL",
+    "Portugal": "PRT",
+    "Romania": "ROU",
+    "Republic of Moldova": "MDA",
+    "Russian Federation": "RUS",
+    "San Marino": "SMR",
+    "Serbia": "SRB",
+    "Slovakia": "SVK",
+    "Slovenia": "SVN",
+    "Spain": "ESP",
+    "Sweden": "SWE",
+    "Switzerland": "CHE",
+    "Tajikistan": "TJK",
+    "Turkey": "TUR",
+    "Turkmenistan": "TKM",
+    "Ukraine": "UKR",
+    "United Kingdom": "GBR",
+    "Uzbekistan": "UZB",
+}
+
 countries = [
     "Albania",
     "Andorra",
@@ -841,10 +899,10 @@ data = data.merge(
     right=pd.DataFrame(
         [
             dict(country=country, **geocode_address(country))
-            for country in countries_dict.values()
+            for country in countries_iso3_dict.values()
         ]
     ),
-    left_on="Geographic area",
+    left_on="REF_AREA",  # was: Geographic area
     right_on="country",
 )
 
--- transmonee_dashboard/src/transmonee_dashboard/pages/base_page.py
+++ transmonee_dashboard/src/transmonee_dashboard/pages/base_page.py
@@ -38,7 +38,7 @@ from . import (
     years,
     data,
     countries_dict_filter,
-    countries_dict,
+    countries_iso3_dict,
 )
 from flask import current_app as server
 
@@ -439,12 +439,8 @@ def display_areas(theme, indicators_dict):
 
 # @cache.memoize()  # will cache based on years and countries combo
 def get_filtered_dataset(theme, years, countries):
-
-    # print("RE-CACHING!!")
-
-    return data[
-        (data["TIME_PERIOD"].isin(years)) & (data["Geographic area"].isin(countries))
-    ]
+    # Use the ref area that contains the countries ISO3 codes to filter the selected countries data
+    return data[(data["TIME_PERIOD"].isin(years)) & (data["REF_AREA"].isin(countries))]
 
 
 @app.callback(
@@ -485,9 +481,8 @@ def apply_filters(theme, years_slider, country_selector, programme_toggle, indic
 
     selected_years = years[slice(*years_slider)]
 
-    # Use the dictionary to return the values of the selected countries based on the SDMX codes
-    countries_selected = countries_dict_filter(countries_dict, countries_selected)
-
+    # Use the dictionary to return the values of the selected countries based on the SDMX ISO3 codes
+    countries_selected = countries_dict_filter(countries_iso3_dict, countries_selected)
     # cache the data based on selected years and countries
     selections = dict(
         theme=theme[1:].upper() if theme else next(iter(indicators.keys())),
