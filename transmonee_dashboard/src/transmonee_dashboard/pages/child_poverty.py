ff045ebee2018b85bc35732c191bdb99efd01a86
lbassil <86417315+lbassil@users.noreply.github.com>
2021-09-30 07:45:59 +0000

    Center all the maps

--- transmonee_dashboard/src/transmonee_dashboard/pages/child_poverty.py
+++ transmonee_dashboard/src/transmonee_dashboard/pages/child_poverty.py
@@ -34,7 +34,7 @@ indicators_dict = {
                 color_continuous_scale=px.colors.sequential.GnBu,
                 mapbox_style="carto-positron",
                 zoom=2,
-                center={"lat": 48.3794, "lon": 31.1656},
+                center={"lat": 62.995158, "lon": 88.048713},
                 opacity=0.5,
                 labels={
                     "OBS_VALUE": "Value",
@@ -176,7 +176,7 @@ indicators_dict = {
                 color_continuous_scale=px.colors.sequential.GnBu,
                 mapbox_style="carto-positron",
                 zoom=2,
-                center={"lat": 48.3794, "lon": 31.1656},
+                center={"lat": 62.995158, "lon": 88.048713},
                 opacity=0.5,
                 labels={
                     "OBS_VALUE": "Value",
