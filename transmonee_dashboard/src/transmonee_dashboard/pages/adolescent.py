00df1d3941b0f055aa34eb0592d50ed2219803e3
lbassil <86417315+lbassil@users.noreply.github.com>
2021-11-04 11:34:42 +0000

    clean imports

--- transmonee_dashboard/src/transmonee_dashboard/pages/adolescent.py
+++ transmonee_dashboard/src/transmonee_dashboard/pages/adolescent.py
@@ -2,8 +2,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, State, Output
-from .base_page import get_base_layout, geo_json_countries
-import plotly.express as px
 
 
 def get_layout(**kwargs):
