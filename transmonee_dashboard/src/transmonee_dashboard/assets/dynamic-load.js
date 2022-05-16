48b8dfebfa5cdc528a6448d15fc8749d23bf2d93
James Cranwell-Ward <jcranwellward@gmail.com>
2022-05-16 21:47:49 +0200

    start work on dynamic load

---
+++ transmonee_dashboard/src/transmonee_dashboard/assets/dynamic-load.js
@@ -0,0 +1,66 @@
+function checkBrowser() {
+    var ES6_Error = false;
+
+    function check() {
+        "use strict";
+
+        if (typeof Symbol == "undefined") return false;
+        try {
+            eval("class A {}");
+            eval("var B = (x) => x+1");
+        } catch (e) {
+            return false;
+        }
+
+        return true;
+    }
+    // The engine supports ES6 features you want to use
+    if (!check()) {
+        ES6_Error = true;
+        document.getElementById('main').style.display = "none";
+        document.getElementById('mainUpdateBrowser').style.display = "";
+        return false;
+    }
+    return true;
+}
+
+//Loads a json file and calls the onsuccess callback, ToDo: add a onerror callback
+function loadJson(url, onsuccess) {
+    var xmlhttp = new XMLHttpRequest();
+    /*xmlhttp.setRequestHeader("Accept", 'application/json');
+    xmlhttp.withCredentials=true;*/
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+            if (xmlhttp.status == 200) {
+                var data = xmlhttp.responseText;
+                onsuccess(data);
+            }
+            else if (xmlhttp.status == 400) {
+                alert('There was an error 400');
+            }
+            else {
+                alert('something else other than 200 was returned');
+            }
+        }
+    };
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+//Checks if the browser is supported 
+var browserOk = checkBrowser();
+
+if (browserOk) {
+
+    //loaad the json and adds to the page in the callback fun
+    loadJson(remote_files_path,
+        function (data) {
+
+            console.log(data);
+            //Load the cfg file
+            var cfg = JSON.parse(data);
+
+        });
+}
